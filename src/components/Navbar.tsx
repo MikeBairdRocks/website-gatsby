e0bead5a1fd87bb004998e1d06c5efc93eb6412a
Michael Baird <mike@baird.rocks>
2021-05-06 14:44:39 -0500

    fixing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ interface NavLinkProps extends NavbarProps {
 }
 
 const NavLink: React.FunctionComponent<NavLinkProps> = (props) => {
-  const linkClass = `text-white hover:text-gray-300 px-4 py-2 flex items-center text-md uppercase font-medium`;
+  const linkClass = `sm:text-white dark:text-white text-black hover:text-gray-300 dark:hover:text-gray-300 px-4 py-2 flex items-center text-md uppercase font-medium`;
 
   return (
     <Link to={props.to} className={linkClass} aria-label={props.text} title={props.text}>{props.children}</Link>
@@ -27,8 +27,6 @@ const NavLink: React.FunctionComponent<NavLinkProps> = (props) => {
 
 const Navbar: React.FunctionComponent<NavbarProps> = (props) => {
   const [navbarOpen, setNavbarOpen] = useState(false);
-  const baseLinkClass = "text-white";
-  const socialClass =  "text-white hover:text-gray-300";
 
   const data = useStaticQuery<SiteQuery>(graphql`
       query Site {
@@ -50,60 +48,58 @@ const Navbar: React.FunctionComponent<NavbarProps> = (props) => {
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+            <button onClick={() => setNavbarOpen(!navbarOpen)} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
 
-              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <svg className={`h-6 w-6 ${navbarOpen ? "hidden" : "block"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
 
-              <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <svg className={`h-6 w-6 ${navbarOpen ? "block" : "hidden"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
-              <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
-                <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
+              <Link to="/">
+                <img className="block lg:hidden h-10 w-auto" src="/logo-icon.svg" alt={data.site?.siteMetadata?.title as string} />
+                <img className="hidden lg:block h-10 w-auto" src="/logo-white-text.svg" alt={data.site?.siteMetadata?.title as string} />
+              </Link>
             </div>
             <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
+              <div className="flex space-x-2">
                 <NavLink {...props} to="/blog" text="Blog">Blog</NavLink>
                 <NavLink {...props} to="/about" text="About">About</NavLink>
               </div>
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span className="sr-only">View notifications</span>
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-              </svg>
-            </button>
-
-            <div className="ml-3 relative">
-              <div>
-                <button type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                </button>
-              </div>
-
-              <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</a>
-              </div>
-            </div>
+            <ul className="flex items-center list-none ml-auto hidden sm:inline-flex">
+              {data.site?.siteMetadata?.social?.map((value, index) => {
+                return (
+                  <li key={`social-${index}`} className="flex items-center text-3xl leading-lg mr-2 p-3">
+                    <SocialIcon color="text-white hover:text-gray-300" type={value?.type as SocialType} href={value?.url ?? ""} text={value?.type?.toString()}/>
+                  </li>
+                );
+              })}
+            </ul>
+            <ThemeToggle />
           </div>
         </div>
       </div>
 
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1">
+      <div className={`sm:hidden transition ease-out duration-500 ${navbarOpen ? "block" : "hidden"}`} id="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1 border-b border-gray-300 dark:border-gray-800">
           <NavLink {...props} to="/blog" text="Blog">Blog</NavLink>
           <NavLink {...props} to="/about" text="About">About</NavLink>
+        </div>
+        <div className="h-24 flex flex-wrap content-center space-x-6 justify-center text-md font-medium">
+          {data.site?.siteMetadata?.social?.map((value, index) => {
+            return (
+              <SocialIcon className="" key={`social-mobile-${index}`} color="dark:text-white text-black hover:text-gray-300" type={value?.type as SocialType} href={value?.url ?? ""} text={value?.type?.toString()} showText={true}/>
+            );
+          })}
         </div>
       </div>
     </nav>
--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, {HTMLAttributes} from "react";
 import {SizeProp} from "@fortawesome/fontawesome-svg-core";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub, faLinkedin, faStackOverflow, faTwitter} from "@fortawesome/free-brands-svg-icons";
 import {SocialType} from "../queries/SiteMetaData";
 
-type SocialIconProps = {
+interface SocialIconProps extends HTMLAttributes<HTMLDivElement> {
   href: string;
   text?: string;
   type: SocialType;
   size?: SizeProp;
   color?: string;
+  showText?: boolean;
 };
 
 const GetIcon = (type: SocialType) => {
@@ -32,16 +33,20 @@ const GetIcon = (type: SocialType) => {
 }
 
 const SocialIcon: React.FunctionComponent<SocialIconProps> = (props) => {
-  const iconClass = `${props.color ?? "text-gray-500"} text-3xl leading-lg`;
+  const iconClass = `${props.color ?? "text-gray-500"}`;
   const size = props.size ?? "1x";
   const icon = GetIcon(props.type);
+  const showText = props.showText ?? false;
+  const text = ` ${showText ? props.text : ""}`;
 
   return (
-    <a href={props.href}
-       aria-label={props.text}
-       className={`${props.color} text-2xl font-normal items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3`}>
-      <span className={iconClass}><FontAwesomeIcon icon={icon} size={size} /></span>
-    </a>
+    <div {...props}>
+      <a href={props.href}
+         aria-label={props.text}
+         className={`${props.color} font-normal rounded-full outline-none focus:outline-none`}>
+        <span className={iconClass}><FontAwesomeIcon icon={icon} size={size} />{text}</span>
+      </a>
+    </div>
   );
 };
 
