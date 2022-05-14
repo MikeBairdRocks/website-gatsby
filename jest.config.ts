6b2ecdb1136874c345327d172e4091cb6faba907
Mike Baird <mike@baird.rocks>
2022-05-14 09:00:04 -0500

    fix

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config.InitialOptions = {
     "^.+\\.tsx?$": `<rootDir>/.jest/jest-preprocess.js`,
   },
   moduleNameMapper: {
+    '^gatsby-page-utils/(.*)$': 'gatsby-page-utils/$1',
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
     "~(.*)$": "<rootDir>/src/$1"
